1c516d84825425cec563d42664c9470ecd5ef1a4
regis <ttf177@gmail.com>
2020-04-13 16:29:30 +1000

    make flowable app standalone

--- mobius-ui/mbu-designer/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('mobiusUi', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
-
-})();
